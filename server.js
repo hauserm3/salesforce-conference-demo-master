918f141bd370fdd744031c192c4ece4f37348bdf
mike-3 <hauserm555@gmail.com>
2016-06-23 19:17:07 +0300

    first

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express(),
+
+    appId = process.env.APP_ID;
+    appId = '3MVG9uudbyLbNPZOn5QWxQzWCJHR1qTxKxwWlRs3C6f7zlRFc6YNAVudKGQEtFDNSMu_3ynGhb4xeNmxLlgc_';
+
+console.log(appId);
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/client'));
+
+
+app.get('/appid', function(req, res) {
+    // console.log('request', req);
+    console.log('response', res);
+    res.send({appId: appId});
+});
+
+app.all('*', function (req, res, next) {
+
+        var targetURL = req.header('Target-URL');
+        if (!targetURL) {
+            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            return;
+        }
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+            }).pipe(res);
+});
+
+app.set('port', process.env.PORT || 8200);
+
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
+});
